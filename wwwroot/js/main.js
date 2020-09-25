bbf7c96322749f473a139ac4366fca60c375e0d6
kurt1288 <3249028+kurt1288@users.noreply.github.com>
2020-09-25 10:40:54 -0700

    Add ability to set alerts

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -132,7 +132,15 @@ const app = new Vue({
         searchResults: [],
         searchTimeout: null,
         db: null,
-        selectedAirport: null
+        selectedAirport: null,
+        showAlerts: false,
+        alertForm: {
+            property: "elapsed",
+            operator: "equals",
+            value: null,
+            eteTimeHelper: null
+        },
+        alerts: []
     },
     watch: {
         acInfo(newData, oldData) {
@@ -166,6 +174,10 @@ const app = new Vue({
 
         this.map = new Map();
 
+        L.easyButton('<span class="material-icons">notifications</span>', () => {
+            this.showAlerts = !this.showAlerts
+        }, 'Alerts').addTo(this.map.map);
+
         const airportsLayer = L.layerGroup();
         this.db.airports.each(airport => {
             airportsLayer.addLayer(L.circleMarker([airport.geolocation.lat, airport.geolocation.lon], { radius: 2 }).on('click', () => {
@@ -181,10 +193,17 @@ const app = new Vue({
     },
     methods: {
         convertSecondsToHMS(value) {
-            if (value < 0)
+            if (!value || isNaN(value) || value < 0)
                 return;
 
             return new Date(value * 1000).toISOString().substr(11, 8)
+        },
+        convertHMSToSeconds(value) {
+            if (!value || !/[0-9]{2}:[0-9]{2}:[0-9]{2}/.test(value))
+                return;
+
+            const sub = value.split(":");
+            return ((parseInt(sub[0]) * 60 + parseInt(sub[1])) * 60) + parseInt(sub[2]);
         },
         apDisplayName(value) {
             switch (value) {
@@ -232,7 +251,7 @@ const app = new Vue({
                 let results = await this.db.airports.where("icao").equalsIgnoreCase(this.searchText).toArray();
                 if (results.length === 0) {
                     const regex = new RegExp(this.searchText, "i");
-                    results = await app.db.airports.filter(airport => regex.test(airport.name)).toArray()
+                    results = await this.db.airports.filter(airport => regex.test(airport.name)).toArray()
                 }
 
                 this.searchResults.push(results);
@@ -245,6 +264,110 @@ const app = new Vue({
             this.searchText = "";
             this.map.map.setView([airport.geolocation.lat, airport.geolocation.lon], 13);
             this.selectedAirport = airport;
+        },
+        async saveAlert() {
+            const permission = await this.requestNotificationPermission();
+            if (!permission)
+                return;
+
+            let value = this.alertForm.value;
+            if (this.alertForm.property !== 'elapsed' && this.alertForm.property !== 'ete')
+                value = parseInt(value);
+
+            let operator = this.alertForm.operator;
+            let unwatch = null;
+
+            switch (this.alertForm.property) {
+                case "elapsed":
+                    value = this.convertHMSToSeconds(value) * 1000;
+                    if (isNaN(value))
+                        return;
+
+                    const timeout = setTimeout(() => {
+                        new Notification(`Elapsed time has passed.`);
+                        this.alerts.splice(this.alerts.findIndex(x => x.type === "elapsed" && x.value === value && x.reference === timeout), 1);
+                    }, value);
+                    this.alerts.push({ name: `Elapsed time ${operator} ${this.convertSecondsToHMS(value / 1000)}`, type: "elapsed", value: value, reference: timeout });
+                    break;
+                case "ete":
+                    value = this.convertHMSToSeconds(value);
+                    if (isNaN(value))
+                        return;
+
+                    unwatch = this.$watch('acInfo.gpswpete', () => {
+                        if ((operator === "equals" && this.acInfo.gpswpete === value) || (operator === "greater" && this.acInfo.gpswpete > value) || (operator === "less" && this.acInfo.gpswpete < value)) {
+                            new Notification(`ETE time is now ${this.convertSecondsToHMS(this.acInfo.gpswpete)}`);
+                            unwatch();
+                            this.alerts.splice(this.alerts.findIndex(x => x.type === "ete" && x.value === value && x.reference === unwatch), 1);
+                        }
+                    }, { deep: true });
+                    this.alerts.push({ name: `ETE ${operator} ${this.convertSecondsToHMS(value)}`, type: "ete", value: value, reference: unwatch });
+                    break;
+                case "wp":
+                    unwatch = this.$watch('acInfo.gpsWaypointDistance', () => {
+                        if ((operator === "equals" && Math.round(this.acInfo.gpsWaypointDistance / 1000) === value) || (operator === "greater" && Math.round(this.acInfo.gpsWaypointDistance / 1000) > value) || (operator === "less" && Math.round(this.acInfo.gpsWaypointDistance / 1000) < value)) {
+                            new Notification(`WP distance is now ${Math.round(this.acInfo.gpsWaypointDistance / 1000)} km`);
+                            unwatch();
+                            this.alerts.splice(this.alerts.findIndex(x => x.type === "wp" && x.value === value && x.reference === unwatch), 1);
+                        }
+                    }, { deep: true });
+                    this.alerts.push({ name: `WP distance ${operator} ${value} km`, type: "wp", value: value, reference: unwatch });
+                    break;
+                case "airspeed":
+                    unwatch = this.$watch('acInfo.airspeedIndicated', () => {
+                        if ((operator === "equals" && Math.round(this.acInfo.airspeedIndicated) === value) || (operator === "greater" && Math.round(this.acInfo.airspeedIndicated) > value) || (operator === "less" && Math.round(this.acInfo.airspeedIndicated) < value)) {
+                            new Notification(`Airspeed is ${Math.round(this.acInfo.airspeedIndicated)} kts`);
+                            unwatch();
+                            this.alerts.splice(this.alerts.findIndex(x => x.type === "airspeed" && x.value === value && x.reference === unwatch), 1);
+                        }
+                    }, { deep: true });
+                    this.alerts.push({ name: `Airspeed ${operator} ${value} kts`, type: "airspeed", value: value, reference: unwatch });
+                    break;
+                case "altitude":
+                    unwatch = this.$watch('acInfo.altitude', () => {
+                        if ((operator === "equals" && Math.round(this.acInfo.altitude) === value) || (operator === "greater" && Math.round(this.acInfo.altitude) > value) || (operator === "less" && Math.round(this.acInfo.altitude) < value)) {
+                            new Notification(`Altitude is ${Math.round(this.acInfo.altitude)} ft`);
+                            unwatch();
+                            this.alerts.splice(this.alerts.findIndex(x => x.type === "altitude" && x.value === value && x.reference === unwatch), 1);
+                        }
+                    }, { deep: true });
+                    this.alerts.push({ name: `Altitude ${operator} ${value} ft`, type: "altitude", value: value, reference: unwatch });
+                    break;
+                case "fuel":
+                    unwatch = this.$watch('acInfo.currentFuel', () => {
+                        if ((operator === "equals" && this.fuelQuantityPercent === value) || (operator === "greater" && this.fuelQuantityPercent > value) || (operator === "less" && this.fuelQuantityPercent < value)) {
+                            new Notification(`Fuel is ${this.fuelQuantityPercent}%`);
+                            unwatch();
+                            this.alerts.splice(this.alerts.findIndex(x => x.type === "fuel" && x.value === value && x.reference === unwatch), 1);
+                        }
+                    }, { deep: true });
+                    this.alerts.push({ name: `Fuel ${operator} ${value}%`, type: "fuel", value: value, reference: unwatch });
+                    break;
+            }
+
+            this.alertForm.value = null;
+            this.showAlerts = false;
+        },
+        removeAlert(alert) {
+            switch (alert.type) {
+                case "elapsed":
+                    clearTimeout(alert.value);
+                    break;
+                default:
+                    alert.reference();
+                    break;
+            }
+
+            this.alerts.splice(this.alerts.findIndex(x => x.value === alert.value), 1);
+        },
+        async requestNotificationPermission() {
+            if (Notification.permission === "granted")
+                return true;
+            else {
+                const permission = await Notification.requestPermission();
+                if (permission === "granted")
+                    return true;
+            }
         }
     },
     computed: {
